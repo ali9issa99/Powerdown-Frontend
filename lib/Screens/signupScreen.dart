7f6b669038cf54f212c91e6462811fb34c7c62ce
ALI9ISSA9 <ali7333170@gmail.com>
2024-09-04 23:25:32 +0300

    added the powerdown title to the signup screen

--- lib/Screens/signupScreen.dart
+++ lib/Screens/signupScreen.dart
@@ -1,2 +1,27 @@
 import 'package:flutter/material.dart';
 
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 20),
+            Text(
+              'Power Down',
+              style: TextStyle(
+                
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
