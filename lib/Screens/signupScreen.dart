e521d158aa225001b7711ac3269ebdf3306d187c
ALI9ISSA9 <ali7333170@gmail.com>
2024-09-04 23:30:10 +0300

    added passwordl text field with it styling

--- lib/Screens/signupScreen.dart
+++ lib/Screens/signupScreen.dart
@@ -66,6 +66,22 @@ class SignUpScreen extends StatelessWidget {
                 ),
               ),
             ),
+             SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                labelStyle: TextStyle(
+                  color: Colors.grey[700],
+                ),
+                filled: true,
+                fillColor: Colors.grey[200],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+              obscureText: true,
+            ),
           ],
         ),
       ),
