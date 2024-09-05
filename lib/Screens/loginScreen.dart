c35babe6d4049fa3d02f18319eb829a8483f575b
ALI9ISSA9 <ali7333170@gmail.com>
2024-09-06 01:31:18 +0300

    added padding to the whole screen

--- lib/Screens/loginScreen.dart
+++ lib/Screens/loginScreen.dart
@@ -10,140 +10,143 @@ class LoginScreen extends StatelessWidget {
       body: SingleChildScrollView( // Wrap with SingleChildScrollView
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(bottom: 50.0),
-                child: Text(
-                  'Power Down',
+          child: Padding(  // Highlight this widget for adding the top padding
+            padding: const EdgeInsets.only(top: 100.0),  // Drop the whole content by 30 pixels
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 50.0),
+                  child: Text(
+                    'Power Down',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Login',
                   style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.w700,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+                const SizedBox(height: 30),
+                const Text(
+                  "Sign in to start optimizing your home's energy efficiency.",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                  textAlign: TextAlign.start,
                 ),
-              ),
-              const SizedBox(height: 30),
-              const Text(
-                "Sign in to start optimizing your home's energy efficiency.",
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black,
+                const SizedBox(height: 20),
+                const Text(
+                  'E-mail',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
                 ),
-                textAlign: TextAlign.start,
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'E-mail',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black,
-                ),
-              ),
-              const SizedBox(height: 8),
-              SizedBox(
-                height: 60, // Adjust height
-                width: double.infinity, // Full width of the container
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'E-mail', // Placeholder
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide.none,
+                const SizedBox(height: 8),
+                SizedBox(
+                  height: 60, // Adjust height
+                  width: double.infinity, // Full width of the container
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'E-mail', // Placeholder
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 16),
                     ),
-                    contentPadding: const EdgeInsets.symmetric(
-                        horizontal: 16, vertical: 16),
                   ),
                 ),
-              ),
-              const SizedBox(height: 16),
-              const Text(
-                'Password',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black,
+                const SizedBox(height: 16),
+                const Text(
+                  'Password',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 8),
-              SizedBox(
-                height: 60, // Adjust height
-                width: double.infinity, // Full width
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: 'Password', // Placeholder
-                    filled: true,
-                    fillColor: Colors.grey[200],
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide.none,
+                const SizedBox(height: 8),
+                SizedBox(
+                  height: 60, // Adjust height
+                  width: double.infinity, // Full width
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Password', // Placeholder
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 16),
                     ),
-                    contentPadding: const EdgeInsets.symmetric(
-                        horizontal: 16, vertical: 16),
                   ),
                 ),
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "Do not have an account?",
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 105, 105, 105)),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SignUpScreen()),
-                      );
-                    },
-                    child: const Text(
-                      'Sign-up',
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Do not have an account?",
                       style: TextStyle(
-                          color: Color.fromARGB(255, 0, 105, 190)),
+                          color: Color.fromARGB(255, 105, 105, 105)),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context,
-                        '/addRoom'); // Navigate to AddRoomScreen
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF004D40),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 165, vertical: 16),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SignUpScreen()),
+                        );
+                      },
+                      child: const Text(
+                        'Sign-up',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 0, 105, 190)),
+                      ),
                     ),
-                  ),
-                  child: const Text(
-                    'Login',
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context,
+                          '/addRoom'); // Navigate to AddRoomScreen
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF004D40),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 165, vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
