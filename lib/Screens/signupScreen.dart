50386c98145bb465a5000d6d9ba988cda86ab172
ALI9ISSA9 <ali7333170@gmail.com>
2024-09-06 01:39:22 +0300

    added new widget to the signupscreen

--- lib/Screens/signupScreen.dart
+++ lib/Screens/signupScreen.dart
@@ -6,114 +6,116 @@ class SignUpScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20),
-            const Text(
-              'Power Down',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Sign up',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              "Looks like you don't have an account. Let's create a new account for you.",
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey[600],
-              ),
-            ),
-            const SizedBox(height: 30),
-            // Name TextField
-            SizedBox(
-              height: 60, // Adjust height
-              width: double.infinity, // Adjust width
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Name', // Placeholder
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide.none,
-                  ),
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      body: SingleChildScrollView( // Wrap with SingleChildScrollView
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 150), // Add more space between top and Power Down
+              const Text(
+                'Power Down',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            // Email TextField
-            SizedBox(
-              height: 60, // Adjust height
-              width: double.infinity, // Adjust width
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'E-mail', // Placeholder
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide.none,
-                  ),
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              const SizedBox(height: 50), // Add more space between Power Down and Sign up
+              const Text(
+                'Sign up',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            // Password TextField
-            SizedBox(
-              height: 60, // Adjust height
-              width: double.infinity, // Adjust width
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: 'Password', // Placeholder
-                  filled: true,
-                  fillColor: Colors.grey[200],
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide.none,
-                  ),
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              const SizedBox(height: 20), // Add more space between Sign up and the sentence
+              Text(
+                "Looks like you don't have an account. Let's create a new account for you.",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[600],
                 ),
               ),
-            ),
-            const SizedBox(height: 30),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Handle sign-up action here
-                },
-                child: const Text('Sign up'),
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white, // Set the text color to white
-                  padding: const EdgeInsets.symmetric(vertical: 15),
-                  backgroundColor: const Color(0xFF004D40),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  textStyle: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+              const SizedBox(height: 30),
+              // Name TextField
+              SizedBox(
+                height: 60, // Adjust height
+                width: double.infinity, // Adjust width
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Name', // Placeholder
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
                   ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              // Email TextField
+              SizedBox(
+                height: 60, // Adjust height
+                width: double.infinity, // Adjust width
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'E-mail', // Placeholder
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              // Password TextField
+              SizedBox(
+                height: 60, // Adjust height
+                width: double.infinity, // Adjust width
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Password', // Placeholder
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Handle sign-up action here
+                  },
+                  child: const Text('Sign up'),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white, // Set the text color to white
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    backgroundColor: const Color(0xFF004D40),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    textStyle: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
