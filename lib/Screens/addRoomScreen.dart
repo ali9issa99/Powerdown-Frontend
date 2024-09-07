91a5930d1bfaac845f10d8aada7748831a2b79f5
ALI9ISSA9 <ali7333170@gmail.com>
2024-09-07 16:47:04 +0300

    removed both device and room modals to widget folder

--- lib/Screens/addRoomScreen.dart
+++ lib/Screens/addRoomScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:powerdown_frontend/widgets/navbar.dart'; // Import the BottomNavBar
+import 'package:powerdown_frontend/widgets/navbar.dart';
+import 'package:powerdown_frontend/widgets/addRoomModal.dart'; // Import AddRoomModal
+import 'package:powerdown_frontend/widgets/addDeviceModal.dart'; // Import AddDeviceModal
 
 class AddRoomScreen extends StatefulWidget {
   const AddRoomScreen({super.key});
@@ -21,112 +23,13 @@ class _AddRoomScreenState extends State<AddRoomScreen> {
   void _showAddRoomModal() {
     showModalBottomSheet(
       context: context,
-      backgroundColor: Colors.transparent, // Makes the background transparent
-      isScrollControlled: true, // To make the modal fullscreen if needed
+      backgroundColor: Colors.transparent,
+      isScrollControlled: true,
       builder: (context) {
         return DraggableScrollableSheet(
           expand: false,
           builder: (context, scrollController) {
-            return Container(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(16),
-                  topRight: Radius.circular(16),
-                ),
-              ),
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 25),
-                  const Align(
-                    alignment: Alignment.topLeft,
-                    child: Text(
-                      'Room',
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 40),
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 15.0),
-                    child: DropdownButtonFormField<String>(
-                      value: 'Living Room',
-                      items: const [
-                        DropdownMenuItem(
-                          value: 'Living Room',
-                          child: Text('Living Room'),
-                        ),
-                        DropdownMenuItem(
-                          value: 'Bedroom',
-                          child: Text('Bedroom'),
-                        ),
-                        DropdownMenuItem(
-                          value: 'Kitchen',
-                          child: Text('Kitchen'),
-                        ),
-                        DropdownMenuItem(
-                          value: 'Guest Room',
-                          child: Text('Guest Room'),
-                        ),
-                        DropdownMenuItem(
-                          value: 'Bathroom',
-                          child: Text('Bathroom'),
-                        ),
-                      ],
-                      onChanged: (value) {},
-                      isExpanded: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 100),
-                  ElevatedButton(
-                    onPressed: () {
-                      _showAddDeviceModal(); // Show add device modal
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF004B43),
-                      minimumSize: const Size.fromHeight(48),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                    ),
-                    child: const Text(
-                      'Continue',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pop(context); // Close the modal
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-                      minimumSize: const Size.fromHeight(48),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                      ),
-                    ),
-                    child: const Text(
-                      'Back',
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ],
-              ),
-            );
+            return const AddRoomModal(); // Use the AddRoomModal widget
           },
         );
       },
@@ -142,7 +45,7 @@ class _AddRoomScreenState extends State<AddRoomScreen> {
         return DraggableScrollableSheet(
           expand: false,
           builder: (context, scrollController) {
-            return AddDeviceModal(); // Custom modal widget for adding device
+            return const AddDeviceModal(); // Use the AddDeviceModal widget
           },
         );
       },
@@ -163,9 +66,7 @@ class _AddRoomScreenState extends State<AddRoomScreen> {
         automaticallyImplyLeading: false, // Removes the back arrow
         actions: [
           IconButton(
-            onPressed: () {
-              _showAddRoomModal(); // Show the modal when plus icon is pressed
-            },
+            onPressed: _showAddRoomModal, // Show the add room modal
             icon: SvgPicture.asset(
               'assets/icons/plus.svg',
               width: 32,
@@ -194,42 +95,6 @@ class _AddRoomScreenState extends State<AddRoomScreen> {
       bottomNavigationBar: BottomNavBar(
         selectedIndex: _selectedIndex,
         onItemTapped: _onItemTapped,
-      ),
-    );
-  }
-}
-
-// Define the AddDeviceModal widget
-class AddDeviceModal extends StatelessWidget {
-  const AddDeviceModal({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(16),
-          topRight: Radius.circular(16),
-        ),
-      ),
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: const [
-          SizedBox(height: 25),
-          Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              'Add Device',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-          // Add device form or UI components here
-        ],
       ),
     );
   }
