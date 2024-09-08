96cdc4e6541d7b667ac4a9e1a8202cc51adb24f0
ALI9ISSA9 <ali7333170@gmail.com>
2024-09-08 23:42:40 +0300

    renaming confirmation.dart into confirmationModal.dart

---
+++ lib/widgets/confirmationModal.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class ConfirmationModal extends StatelessWidget {
+  const ConfirmationModal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      expand: false,
+      builder: (context, scrollController) {
+        return Container(
+          padding: const EdgeInsets.all(16.0),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(16),
+              topRight: Radius.circular(16),
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(height: 25),
+              const Icon(
+                Icons.check_circle,
+                color: Colors.green,
+                size: 70,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Living Room Added!',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context); // Closes the modal
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF004B43),
+                  minimumSize: const Size.fromHeight(48),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                ),
+                child: const Text(
+                  'Done',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
