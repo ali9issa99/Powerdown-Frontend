e24fa6500b60b5f123d92558f40714eb6e8e14a8
ALI9ISSA9 <ali7333170@gmail.com>
2024-09-11 00:40:36 +0300

    added a white background instead of a light green one

--- lib/Screens/overviewScreen.dart
+++ lib/Screens/overviewScreen.dart
@@ -9,6 +9,7 @@ class OverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(
           'Energy Usage Overview',
