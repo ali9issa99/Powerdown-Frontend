b0e982a1d8edaa71ae746a08d06fa27769fb2c48
ALI9ISSA9 <ali7333170@gmail.com>
2024-09-12 21:28:06 +0300

    added the provider to the addRoomModal.dart

--- lib/widgets/addRoomModal.dart
+++ lib/widgets/addRoomModal.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:powerdown_frontend/widgets/addDeviceModal.dart'; // Import the AddDeviceModal
+import 'package:powerdown_frontend/provider/roomProvider.dart';
+import 'package:powerdown_frontend/widgets/addDeviceModal.dart';
+import 'package:provider/provider.dart'; // Import the AddDeviceModal
 
 class AddRoomModal extends StatefulWidget {
   const AddRoomModal({Key? key}) : super(key: key);
@@ -13,6 +15,7 @@ class _AddRoomModalState extends State<AddRoomModal> {
 
   @override
   Widget build(BuildContext context) {
+    final room = Provider.of<deviceProvider>(context);
     return Container(
       decoration: const BoxDecoration(
         color: Colors.white,
@@ -22,119 +25,121 @@ class _AddRoomModalState extends State<AddRoomModal> {
         ),
       ),
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 25),
-          const Align(
-            alignment: Alignment.topLeft,
-            child: Text(
-              'Room',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-          const SizedBox(height: 40),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 15.0),
-            color: Colors.white,
-            child: DropdownButtonFormField<String>(
-              value: _selectedRoom,
-              dropdownColor: Colors.white,
-              items: const [
-                DropdownMenuItem(
-                  value: 'Living Room',
-                  child: Text('Living Room'),
-                ),
-                DropdownMenuItem(
-                  value: 'Bedroom',
-                  child: Text('Bedroom'),
-                ),
-                DropdownMenuItem(
-                  value: 'Kitchen',
-                  child: Text('Kitchen'),
-                ),
-                DropdownMenuItem(
-                  value: 'Guest Room',
-                  child: Text('Guest Room'),
-                ),
-                DropdownMenuItem(
-                  value: 'Bathroom',
-                  child: Text('Bathroom'),
-                ),
-              ],
-              onChanged: (value) {
-                setState(() {
-                  _selectedRoom = value!;
-                });
-              },
-              isExpanded: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+    
+        child:  Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 25),
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Room',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w500,
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 100),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context); // Close the current modal
-
-              // Open AddDeviceModal and pass the selected room
-              showModalBottomSheet(
-                context: context,
-                backgroundColor: Colors.transparent,
-                isScrollControlled: true,
-                builder: (context) {
-                  return DraggableScrollableSheet(
-                    expand: false,
-                    builder: (context, scrollController) {
-                      return AddDeviceModal(selectedRoom: _selectedRoom); // Pass the room
-                    },
-                  );
+            const SizedBox(height: 40),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 15.0),
+              color: Colors.white,
+              child: DropdownButtonFormField<String>(
+                value: _selectedRoom,
+                dropdownColor: Colors.white,
+                items: const [
+                  DropdownMenuItem(
+                    value: 'Living Room',
+                    child: Text('Living Room'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Bedroom',
+                    child: Text('Bedroom'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Kitchen',
+                    child: Text('Kitchen'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Guest Room',
+                    child: Text('Guest Room'),
+                  ),
+                  DropdownMenuItem(
+                    value: 'Bathroom',
+                    child: Text('Bathroom'),
+                  ),
+                ],
+                onChanged: (value) {
+        
+                  room.setSelectedRoom = value!;
                 },
-              );
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xFF004B43),
-              minimumSize: const Size.fromHeight(48),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(25.0),
+                isExpanded: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
               ),
             ),
-            child: const Text(
-              'Continue',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
+            const SizedBox(height: 100),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the current modal
+        
+                // Open AddDeviceModal and pass the selected room
+                showModalBottomSheet(
+                  context: context,
+                  backgroundColor: Colors.transparent,
+                  isScrollControlled: true,
+                  builder: (context) {
+                    return DraggableScrollableSheet(
+                      expand: false,
+                      builder: (context, scrollController) {
+                        return AddDeviceModal(selectedRoom: _selectedRoom); // Pass the room
+                      },
+                    );
+                  },
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF004B43),
+                minimumSize: const Size.fromHeight(48),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+              ),
+              child: const Text(
+                'Continue',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 16),
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context); // Close the modal
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.white,
-              minimumSize: const Size.fromHeight(48),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(25.0),
-                side: const BorderSide(color: Color.fromARGB(255, 255, 255, 255)),
+            const SizedBox(height: 16),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the modal
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                minimumSize: const Size.fromHeight(48),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                  side: const BorderSide(color: Color.fromARGB(255, 255, 255, 255)),
+                ),
+              ),
+              child: const Text(
+                'Back',
+                style: TextStyle(fontSize: 18),
               ),
             ),
-            child: const Text(
-              'Back',
-              style: TextStyle(fontSize: 18),
-            ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ));
+
+      
+       
   }
 }
 
