553b642c149f31e18f51f9e07b036429bc084eee
ALI9ISSA9 <ali7333170@gmail.com>
2024-09-14 03:11:00 +0300

    removed unnecessary previously commented code from Room modal

--- lib/widgets/addRoomModal.dart
+++ lib/widgets/addRoomModal.dart
@@ -1,158 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:powerdown_frontend/provider/roomProvider.dart';
-// import 'package:powerdown_frontend/widgets/addDeviceModal.dart';
-// import 'package:powerdown_frontend/widgets/confirmationModal.dart'; // Import the ConfirmationModal
-// import 'package:provider/provider.dart';
-
-// class AddRoomModal extends StatefulWidget {
-//   final void Function(String) onRoomSelected;
-
-//   const AddRoomModal({Key? key, required this.onRoomSelected}) : super(key: key);
-
-//   @override
-//   State<AddRoomModal> createState() => _AddRoomModalState();
-// }
-
-// class _AddRoomModalState extends State<AddRoomModal> {
-//   String _selectedRoom = 'Living Room'; // Default value
-//   String _pendingRoom = '';  // To store the room temporarily
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final room = Provider.of<deviceProvider>(context);
-//     return Container(
-//         decoration: const BoxDecoration(
-//           color: Colors.white,
-//           borderRadius: BorderRadius.only(
-//             topLeft: Radius.circular(16),
-//             topRight: Radius.circular(16),
-//           ),
-//         ),
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           mainAxisSize: MainAxisSize.min,
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             const SizedBox(height: 25),
-//             const Align(
-//               alignment: Alignment.topLeft,
-//               child: Text(
-//                 'Room',
-//                 style: TextStyle(
-//                   fontSize: 28,
-//                   fontWeight: FontWeight.w500,
-//                 ),
-//               ),
-//             ),
-//             const SizedBox(height: 40),
-//             Container(
-//               padding: const EdgeInsets.symmetric(vertical: 15.0),
-//               color: Colors.white,
-//               child: DropdownButtonFormField<String>(
-//                 value: _selectedRoom,
-//                 dropdownColor: Colors.white,
-//                 items: const [
-//                   DropdownMenuItem(
-//                     value: 'Living Room',
-//                     child: Text('Living Room'),
-//                   ),
-//                   DropdownMenuItem(
-//                     value: 'Bedroom',
-//                     child: Text('Bedroom'),
-//                   ),
-//                   DropdownMenuItem(
-//                     value: 'Kitchen',
-//                     child: Text('Kitchen'),
-//                   ),
-//                   DropdownMenuItem(
-//                     value: 'Guest Room',
-//                     child: Text('Guest Room'),
-//                   ),
-//                   DropdownMenuItem(
-//                     value: 'Bathroom',
-//                     child: Text('Bathroom'),
-//                   ),
-//                 ],
-//                 onChanged: (value) {
-//                   setState(() {
-//                     _selectedRoom = value!;
-//                   });
-//                 },
-//                 isExpanded: true,
-//                 decoration: InputDecoration(
-//                   border: OutlineInputBorder(
-//                     borderRadius: BorderRadius.circular(10.0),
-//                   ),
-//                 ),
-//               ),
-//             ),
-//             const SizedBox(height: 100),
-//             ElevatedButton(
-//               onPressed: () {
-//                 setState(() {
-//                   _pendingRoom = _selectedRoom; // Store the selected room
-//                 });
-//                 Navigator.pop(context); // Close the AddRoomModal
-
-//                 // Navigate to AddDeviceModal
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(
-//                     builder: (context) => AddDeviceModal(
-//                       selectedRoom: _pendingRoom,
-//                       onDeviceAdded: (deviceName) {
-//                         // Navigate to ConfirmationModal
-//                         Navigator.push(
-//                           context,
-//                           MaterialPageRoute(
-//                             builder: (context) => ConfirmationModal(
-//                               selectedRoom: _pendingRoom,
-//                               onRoomAdded: (roomName) {
-//                                 room.addRoom(roomName, ''); // Add the room when done is pressed
-//                                 Navigator.pop(context); // Close ConfirmationModal
-//                               },
-//                             ),
-//                           ),
-//                         );
-//                       },
-//                     ),
-//                   ),
-//                 );
-//               },
-//               style: ElevatedButton.styleFrom(
-//                 backgroundColor: const Color(0xFF004B43),
-//                 minimumSize: const Size.fromHeight(48),
-//                 shape: RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(25.0),
-//                 ),
-//               ),
-//               child: const Text(
-//                 'Continue',
-//                 style: TextStyle(
-//                   color: Colors.white,
-//                   fontSize: 18,
-//                 ),
-//               ),
-//             ),
-//             const SizedBox(height: 16),
-//             TextButton(
-//               onPressed: () {
-//                 Navigator.pop(context); // Close the AddRoomModal
-//               },
-//               style: TextButton.styleFrom(
-//                 minimumSize: const Size.fromHeight(48),
-//               ),
-//               child: const Text(
-//                 'Back',
-//                 style: TextStyle(
-//                     fontSize: 18, color: Color.fromARGB(255, 0, 75, 67)),
-//               ),
-//             ),
-//           ],
-//         ));
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:powerdown_frontend/provider/roomProvider.dart';
 // import 'package:powerdown_frontend/widgets/addDeviceModal.dart';
@@ -162,7 +7,8 @@ import 'package:provider/provider.dart';
 class AddRoomModal extends StatefulWidget {
   final void Function(String) onRoomSelected;
 
-  const AddRoomModal({Key? key, required this.onRoomSelected}) : super(key: key);
+  const AddRoomModal({Key? key, required this.onRoomSelected})
+      : super(key: key);
 
   @override
   State<AddRoomModal> createState() => _AddRoomModalState();
@@ -170,7 +16,7 @@ class AddRoomModal extends StatefulWidget {
 
 class _AddRoomModalState extends State<AddRoomModal> {
   String _selectedRoom = 'Living Room'; // Default value
-  String _pendingRoom = '';  // To store the room temporarily
+  String _pendingRoom = ''; // To store the room temporarily
 
   @override
   Widget build(BuildContext context) {
@@ -247,7 +93,6 @@ class _AddRoomModalState extends State<AddRoomModal> {
                 setState(() {
                   _pendingRoom = _selectedRoom; // Store the selected room
                 });
-                widget.onRoomSelected(_pendingRoom);
                 // Open the confirmation modal
                 showModalBottomSheet(
                   context: context,
@@ -257,7 +102,8 @@ class _AddRoomModalState extends State<AddRoomModal> {
                     return ConfirmationModal(
                       selectedRoom: _pendingRoom,
                       onRoomAdded: (roomName) {
-                        room.addRoom(roomName, ''); // Add the room when done is pressed
+                        room.addRoom(
+                            roomName, ''); // Add the room when done is pressed
                       },
                     );
                   },
