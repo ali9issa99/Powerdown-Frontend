e262fa4ccdf4c69bf001ba6ddbc4f8aea42d02f6
ALI9ISSA9 <ali7333170@gmail.com>
2024-09-14 23:55:46 +0300

    worked on fixing the bottomsheets and the roomcard

--- lib/widgets/confirmationModal.dart
+++ lib/widgets/confirmationModal.dart
@@ -1,77 +1,107 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:powerdown_frontend/provider/roomProvider.dart';
 
-// class ConfirmationModal extends StatelessWidget {
-//   final String selectedRoom;
-//   final Function(String roomName)? onRoomAdded;
 
-//   const ConfirmationModal({
-//     Key? key,
-//     required this.selectedRoom,
-//     this.onRoomAdded, // Update constructor
-//   }) : super(key: key);
+class ConfirmationModal extends StatelessWidget {
+  final String selectedRoom;
+  final Function(String roomName)? onRoomAdded;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return DraggableScrollableSheet(
-//       expand: false,
-//       builder: (context, scrollController) {
-//         return Container(
-//           padding: const EdgeInsets.all(16.0),
-//           decoration: const BoxDecoration(
-//             color: Colors.white,
-//             borderRadius: BorderRadius.only(
-//               topLeft: Radius.circular(16),
-//               topRight: Radius.circular(16),
-//             ),
-//           ),
-//           child: Column(
-//             mainAxisSize: MainAxisSize.min,
-//             children: [
-//               const SizedBox(height: 40),
-//               const Icon(
-//                 Icons.task_alt_outlined,
-//                 color: Color.fromARGB(255, 88, 146, 50),
-//                 size: 140,
-//               ),
-//               const SizedBox(height: 45),
-//               Text(
-//                 '$selectedRoom was Added!',
-//                 style: const TextStyle(
-//                   fontSize: 28,
-//                   fontWeight: FontWeight.w600,
-//                 ),
-//               ),
-//               const SizedBox(height: 75),
-//               ElevatedButton(
-//                 onPressed: () {
-//                   if (onRoomAdded != null) {
-//                     onRoomAdded!(
-//                         selectedRoom); // Call the function if it's not null
-//                   }
-//                   Navigator.pop(context); // Close the modal
-//                 },
-//                 style: ElevatedButton.styleFrom(
-//                   backgroundColor: const Color(0xFF004B43),
-//                   minimumSize: const Size.fromHeight(48),
-//                   shape: RoundedRectangleBorder(
-//                     borderRadius: BorderRadius.circular(25.0),
-//                   ),
-//                 ),
-//                 child: const Text(
-//                   'Done',
-//                   style: TextStyle(
-//                     color: Colors.white,
-//                     fontSize: 18,
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
+  const ConfirmationModal({
+    Key? key,
+    required this.selectedRoom,
+    this.onRoomAdded,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      expand: false,
+      builder: (context, scrollController) {
+        return Container(
+          padding: const EdgeInsets.all(16.0),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(16),
+              topRight: Radius.circular(16),
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(height: 40),
+              const Icon(
+                Icons.task_alt_outlined,
+                color: Color.fromARGB(255, 88, 146, 50),
+                size: 140,
+              ),
+              const SizedBox(height: 45),
+              Text(
+                '$selectedRoom was Added!',
+                style: const TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                  decoration: TextDecoration.none,
+                ),
+              ),
+              const SizedBox(height: 75),
+              ElevatedButton(
+                onPressed: () {
+                  if (onRoomAdded != null) {
+                    onRoomAdded!(selectedRoom); // Add room to provider
+                    // Add room to provider
+                    Provider.of<deviceProvider>(context, listen: false)
+                        .addRoom(selectedRoom, _getRoomImagePath(selectedRoom));
+                  }
+                  Navigator.pop(context);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF004B43),
+                  minimumSize: const Size.fromHeight(48),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                ),
+                child: const Text(
+                  'Done',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  String _getRoomImagePath(String roomName) {
+    switch (roomName) {
+      case 'Living Room':
+        return 'assets/images/livingRoom.png';
+      case 'Bedroom':
+        return 'assets/images/bedroom.png';
+      case 'Kitchen':
+        return 'assets/images/kitchen.png';
+      case 'Guest Room':
+        return 'assets/images/guestRoom.png';
+      case 'Bathroom':
+        return 'assets/images/bathroom.png';
+      default:
+        return 'assets/images/defaultRoom.png';
+    }
+  }
+}
+
+
+
+
+
+
 
 
 
@@ -145,77 +175,80 @@
 
 
 
-import 'package:flutter/material.dart';
 
-class ConfirmationModal extends StatelessWidget {
-  final String selectedRoom;
-  final Function(String roomName)? onRoomAdded;
 
-  const ConfirmationModal({
-    Key? key,
-    required this.selectedRoom,
-    this.onRoomAdded, // Update constructor
-  }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return DraggableScrollableSheet(
-      expand: false,
-      builder: (context, scrollController) {
-        return Container(
-          padding: const EdgeInsets.all(16.0),
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(16),
-              topRight: Radius.circular(16),
-            ),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const SizedBox(height: 40),
-              const Icon(
-                Icons.task_alt_outlined,
-                color: Color.fromARGB(255, 88, 146, 50),
-                size: 140,
-              ),
-              const SizedBox(height: 45),
-              Text(
-                '$selectedRoom was Added!',
-                style: const TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              const SizedBox(height: 75),
-              ElevatedButton(
-                onPressed: () {
-                  if (onRoomAdded != null) {
-                    onRoomAdded!(
-                        selectedRoom); // Call the function if it's not null
-                  }
-                  Navigator.pop(context); // Close the modal
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF004B43),
-                  minimumSize: const Size.fromHeight(48),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25.0),
-                  ),
-                ),
-                child: const Text(
-                  'Done',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-}
+
+// import 'package:flutter/material.dart';
+
+// class ConfirmationModal extends StatelessWidget {
+//   final String selectedRoom;
+//   final Function(String roomName)? onRoomAdded;
+
+//   const ConfirmationModal({
+//     Key? key,
+//     required this.selectedRoom,
+//     this.onRoomAdded, // Update constructor
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return DraggableScrollableSheet(
+//       expand: false,
+//       builder: (context, scrollController) {
+//         return Container(
+//           padding: const EdgeInsets.all(16.0),
+//           decoration: const BoxDecoration(
+//             color: Colors.white,
+//             borderRadius: BorderRadius.only(
+//               topLeft: Radius.circular(16),
+//               topRight: Radius.circular(16),
+//             ),
+//           ),
+//           child: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             children: [
+//               const SizedBox(height: 40),
+//               const Icon(
+//                 Icons.task_alt_outlined,
+//                 color: Color.fromARGB(255, 88, 146, 50),
+//                 size: 140,
+//               ),
+//               const SizedBox(height: 45),
+//               Text(
+//                 '$selectedRoom was Added!',
+//                 style: const TextStyle(
+//                   fontSize: 28,
+//                   fontWeight: FontWeight.w600,
+//                 ),
+//               ),
+//               const SizedBox(height: 75),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   if (onRoomAdded != null) {
+//                     onRoomAdded!(selectedRoom); // Add room logic here
+//                   }
+//                   Navigator.pop(context); // Close ConfirmationModal
+//                 },
+//                 style: ElevatedButton.styleFrom(
+//                   backgroundColor: const Color(0xFF004B43),
+//                   minimumSize: const Size.fromHeight(48),
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(25.0),
+//                   ),
+//                 ),
+//                 child: const Text(
+//                   'Done',
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     fontSize: 18,
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
