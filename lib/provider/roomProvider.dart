43885de52a6d379a40498eccb2206776bce9e000
ALI9ISSA9 <ali7333170@gmail.com>
2024-09-14 03:02:53 +0300

    deleted un necessary commented code

--- lib/provider/roomProvider.dart
+++ lib/provider/roomProvider.dart
@@ -1,25 +1,48 @@
 import 'package:flutter/material.dart';
 
 class RoomProvider extends ChangeNotifier {
-  List selectedRooms = [];
+  List<String> selectedRooms = [];
 
   get getSelectedRoom => selectedRooms;
 
   set setSelectedRoom(String newRoom) {
     selectedRooms.add(newRoom);
-
     notifyListeners();
   }
 }
 
-class deviceProvider extends ChangeNotifier {
-  List selectedRooms = [];
 
-  get getSelectedRoom => selectedRooms;
 
-  set setSelectedRoom(String deviceImage) {
-    selectedRooms.add(deviceImage);
+class deviceProvider with ChangeNotifier {
+  List<Map<String, String>> selectedRooms = [];
 
+  void addRoom(String roomName, String imagePath) {
+    selectedRooms.add({
+      'name': roomName,
+      'imagePath': imagePath,
+    });
+    notifyListeners();
+  }
+
+  void removeRoom(int index) {
+    selectedRooms.removeAt(index);
     notifyListeners();
   }
 }
+
+
+
+
+
+// class deviceProvider with ChangeNotifier {
+//   String _selectedRoom = 'Living Room'; // Default value for the room
+
+//   // Getter to access the current selected room
+//   String get selectedRoom => _selectedRoom;
+
+//   // Setter to update the selected room and notify listeners
+//   set setSelectedRoom(String room) {
+//     _selectedRoom = room;
+//     notifyListeners(); // Notify listeners to update the UI
+//   }
+// }
