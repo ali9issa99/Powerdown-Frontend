6958f528ac9c731a353699799121cc6ef81bbba6
ALI9ISSA9 <ali7333170@gmail.com>
2024-10-09 18:29:54 +0300

    fixed roomProvider

--- lib/provider/roomProvider.dart
+++ lib/provider/roomProvider.dart
@@ -2,82 +2,81 @@ import 'package:flutter/material.dart';
 import 'package:powerdown_frontend/models/roomModel.dart';
 import 'package:uuid/uuid.dart'; // Add this package for unique IDs
 
-class RoomProvider extends ChangeNotifier {
-  List<String> selectedRooms = [];
 
-  get getSelectedRoom => selectedRooms;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-  set setSelectedRoom(String newRoom) {
-    selectedRooms.add(newRoom);
+class UserProvider extends ChangeNotifier {
+  User _user;
+
+  UserProvider(this._user);
+
+  User get user => _user;
+
+  set user(User newUser) {
+    _user = newUser;
     notifyListeners();
+  }
+
+  // Fetch user data from backend
+  Future<void> fetchUserData(String userId) async {
+    try {
+      final response = await http.get(Uri.parse('https://10.0.2.2:8080/api/users/$userId'));
+      if (response.statusCode == 200) {
+        _user = User.fromJson(json.decode(response.body));
+        notifyListeners();
+      } else {
+        throw Exception('Failed to load user');
+      }
+    } catch (error) {
+      print('Error fetching user: $error');
+    }
+  }
+
+  // Update user data in backend
+  Future<void> updateUser(User updatedUser) async {
+    try {
+      final response = await http.put(
+        Uri.parse('https://10.0.2.2:8080/api/users/${updatedUser.userId}'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(updatedUser.toJson()),
+      );
+      if (response.statusCode == 200) {
+        _user = User.fromJson(json.decode(response.body));
+        notifyListeners();
+      } else {
+        throw Exception('Failed to update user');
+      }
+    } catch (error) {
+      print('Error updating user: $error');
+    }
+  }
+
+  // Add new room
+  Future<void> addRoom(Room room) async {
+    try {
+      final response = await http.post(
+        Uri.parse('https://10.0.2.2:8080/api/users/${_user.userId}/rooms'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({'action': 'addRoom', 'data': {'roomName': room.name}}),
+      );
+      if (response.statusCode == 200) {
+        _user.rooms.add(Room.fromJson(json.decode(response.body)));
+        notifyListeners();
+      }
+    } catch (error) {
+      print('Error adding room: $error');
+    }
   }
 }
 
-// class deviceProvider with ChangeNotifier {
-//   List<Map<String, String>> selectedRooms = [];
 
-//   // Method to add a room
-//   void addRoom(String roomName, String imagePath) {
-//     selectedRooms.add({
-//       'name': roomName,
-//       'imagePath': imagePath,
-//     });
-//     notifyListeners(); // Notify listeners that the list has changed
-//   }
-
-//   // Method to remove a room
-//   void removeRoom(int index) {
-//     selectedRooms.removeAt(index);
-//     notifyListeners();
-//   }
-// }
-
-// class deviceProvider with ChangeNotifier {
-//   List<Map<String, dynamic>> selectedRooms = [];
-
-//   // Method to add a room
-//   void addRoom(String roomName, String imagePath) {
-//     selectedRooms.add({
-//       'name': roomName,
-//       'imagePath': imagePath,
-//       'devices': [], // Initialize an empty list of devices for the room
-//     });
-//     notifyListeners(); // Notify listeners that the list has changed
-//   }
-
-//   // Method to remove a room
-//   void removeRoom(int index) {
-//     selectedRooms.removeAt(index);
-//     notifyListeners();
-//   }
-
-//   // Method to add a device to a room
-//   void addDeviceToRoom(String roomName, String deviceName) {
-//     final room = selectedRooms.firstWhere((room) => room['name'] == roomName);
-//     if (!room['devices'].contains(deviceName)) {
-//       room['devices'].add(deviceName);
-//       notifyListeners(); // Trigger UI update
-//     }
-//   }
-
-//   // Method to remove a device from a room
-//   void removeDeviceFromRoom(String roomName, String deviceName) {
-//     final room = selectedRooms.firstWhere((room) => room['name'] == roomName);
-//     room['devices'].remove(deviceName);
-//     notifyListeners(); // Trigger UI update
-//   }
-// }
-
-class DeviceProvider with ChangeNotifier {
-  final Uuid uuid = Uuid(); // For generating unique IDs
+class RoomProvider with ChangeNotifier {
+  final Uuid uuid = Uuid();
   List<Room> selectedRooms = [];
 
   Room? roomToAdd;
   void setRoomName(String roomName) {
-    // roomToAdd["id"] = uuid.v4();
-    // roomToAdd["name"] = roomName;
-    // roomToAdd['devices'] = [];
-    // roomToAdd['imagePath'] = _getRoomImagePath(roomName);
     roomToAdd = Room(
       id: uuid.v4(),
       name: roomName,
@@ -86,16 +85,6 @@ class DeviceProvider with ChangeNotifier {
     );
   }
 
-  // // Add unique ID to each room and ensure 'devices' is initialized as an empty list
-  // void addRoom(String roomName, String imagePath) {
-  //   selectedRooms.add({
-  //     'id': uuid.v4(), // Generate a unique ID
-  //     'name': roomName,
-  //     'imagePath': imagePath,
-  //     'devices': <String>[], // Initialize an empty list of devices
-  //   });
-  //   notifyListeners();
-  // }
   String _getRoomImagePath(String roomName) {
     switch (roomName) {
       case 'Living Room':
@@ -114,35 +103,23 @@ class DeviceProvider with ChangeNotifier {
   }
 
   String addRoom(String roomName, String imagePath) {
-    final roomId = uuid.v4(); // Generate a unique ID
-    selectedRooms.add(Room(id: roomId, name: roomName, imagePath: imagePath, devices: []));
+    final roomId = uuid.v4();
+    selectedRooms.add(
+        Room(id: roomId, name: roomName, imagePath: imagePath, devices: []));
     notifyListeners();
     return roomId;
   }
 
   void removeRoom(String roomId) {
     selectedRooms.removeWhere((room) => room.id == roomId);
-    // selectedRooms = List<Map<String, dynamic>>.from(
-        // selectedRooms); // Ensure list is replaced
-        
+
     notifyListeners();
   }
 
   // Updated methods with list replacement
   void addDeviceToRoom(String roomId, String deviceName) {
-    // final roomIndex = selectedRooms.indexWhere((room) => room['id'] == roomId);
-    // print(roomIndex);
-    // if (roomIndex != -1) {
-    //   Map<String, dynamic> room = Map<String, dynamic>.from(selectedRooms[roomIndex]);
-    //   List<String> devices = List<String>.from(room['devices'] as List<String>? ?? <String>[]);
-
-    // print(devices);
     if (!roomToAdd!.devices.contains(deviceName)) {
       roomToAdd!.devices.add(deviceName);
-      // room['devices'] = devices;
-      // selectedRooms[roomIndex] = room;
-      // selectedRooms = List<Map<String, dynamic>>.from(selectedRooms); // Replace entire list
-      // print(room);
     }
   }
 
@@ -150,26 +127,6 @@ class DeviceProvider with ChangeNotifier {
     selectedRooms.add(roomToAdd!);
     notifyListeners();
   }
-
-  // void removeDeviceFromRoom(String roomId, String deviceName) {
-  //   final roomIndex = selectedRooms.indexWhere((room) => room.id == roomId);
-  //   if (roomIndex != -1) {
-  //     List<Map<String, dynamic>> roomsClone =
-  //         List<Map<String, dynamic>>.from(selectedRooms);
-  //     Map<String, dynamic> room =
-  //         Map<String, dynamic>.from(roomsClone[roomIndex]);
-  //     List<String> devices =
-  //         List<String>.from(room['devices'] as List<String>? ?? <String>[]);
-
-  //     if (devices.contains(deviceName)) {
-  //       devices.remove(deviceName);
-  //       room['devices'] = devices;
-  //       roomsClone[roomIndex] = room;
-  //       selectedRooms = roomsClone;
-  //       notifyListeners();
-  //     }
-  //   }
-  // }
 
   List<String> getDevicesForRoom(String roomId) {
     final roomIndex = selectedRooms.indexWhere((room) => room.id == roomId);
@@ -179,4 +136,206 @@ class DeviceProvider with ChangeNotifier {
     }
     return <String>[];
   }
+
+
+
+
+ // Backend Integration Methods
+
+  // Fetch rooms from backend
+  Future<void> fetchRooms(String userId) async {
+    try {
+      final response = await http.get(Uri.parse('https://10.0.2.2:8080/api/users/$userId/rooms'));
+      if (response.statusCode == 200) {
+        final List<dynamic> roomsData = json.decode(response.body);
+        selectedRooms = roomsData.map((room) => Room.fromJson(room)).toList();
+        notifyListeners();
+      } else {
+        throw Exception('Failed to load rooms');
+      }
+    } catch (error) {
+      print('Error fetching rooms: $error');
+    }
+  }
+
+  // Add room to backend
+// Add room to backend
+Future<void> addRoomToBackend(String roomName, String userId) async {
+  final roomId = uuid.v4();
+  final newRoom = Room(
+    id: roomId,
+    name: roomName,
+    imagePath: _getRoomImagePath(roomName),
+    devices: [],
+  );
+
+  try {
+    final response = await http.post(
+      Uri.parse('https://10.0.2.2:8080/api/users/$userId/rooms'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({'action': 'addRoom', 'data': {'roomName': roomName}}),
+    );
+    if (response.statusCode == 200) {
+      // Update frontend state with new room
+      selectedRooms.add(newRoom);
+      notifyListeners();
+    } else {
+      throw Exception('Failed to add room');
+    }
+  } catch (error) {
+    print('Error adding room: $error');
+  }
+}
+
+
+  // Remove room from backend
+  Future<void> removeRoomFromBackend(String roomId, String userId) async {
+    try {
+      final response = await http.delete(
+        Uri.parse('https://10.0.2.2:8080/api/users/$userId/rooms/$roomId'),
+      );
+      if (response.statusCode == 200) {
+        removeRoom(roomId);
+      } else {
+        throw Exception('Failed to remove room');
+      }
+    } catch (error) {
+      print('Error removing room: $error');
+    }
+  }
+
+// Add device to a room in the backend
+Future<void> addDeviceToBackendRoom(String roomId, String deviceName, String userId) async {
+  try {
+    final response = await http.post(
+      Uri.parse('https://10.0.2.2:8080/api/users/$userId/rooms/$roomId/devices'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({'action': 'addDevice', 'data': {'roomName': roomId, 'deviceName': deviceName}}),
+    );
+    if (response.statusCode == 200) {
+      // Update frontend state after successful addition to backend
+      addDeviceToRoom(roomId, deviceName);
+      notifyListeners();
+    } else {
+      throw Exception('Failed to add device');
+    }
+  } catch (error) {
+    print('Error adding device: $error');
+  }
+}
+
+
+
+
+
+
+
+
+
+
+// class DeviceProvider with ChangeNotifier {
+//   List<Device> devices = [];
+
+//   void addDevice(String deviceId, String deviceName, String status) {
+//     devices.add(Device(
+//       deviceId: deviceId,
+//       deviceName: deviceName,
+//       status: status,
+//       consumption: [], // Initialize an empty list for consumption details
+//     ));
+//     notifyListeners(); // Notify the listeners to update UI
+//   }
+
+//   void removeDevice(String deviceId) {
+//     devices.removeWhere((device) => device.deviceId == deviceId);
+//     notifyListeners(); // Notify the listeners to update UI
+//   }
+
+//   void toggleDeviceStatus(String deviceId) {
+//     final deviceIndex = devices.indexWhere((device) => device.deviceId == deviceId);
+//     if (deviceIndex != -1) {
+//       devices[deviceIndex].status = (devices[deviceIndex].status == 'on') ? 'off' : 'on';
+//       notifyListeners(); // Notify the listeners to update UI
+//     }
+//   }
+
+//   void addConsumption(String deviceId, String consumptionDetail) {
+//     final deviceIndex = devices.indexWhere((device) => device.deviceId == deviceId);
+//     if (deviceIndex != -1) {
+//       devices[deviceIndex].consumption.add(consumptionDetail);
+//       notifyListeners(); // Notify the listeners to update UI
+//     }
+//   }
+
+//   List<String> getDeviceConsumption(String deviceId) {
+//     final device = devices.firstWhere((device) => device.deviceId == deviceId, orElse: () => Device(deviceId: '', deviceName: '', status: '', consumption: []));
+//     return device.consumption;
+//   }
+// }
+
+//###############################################################################################
+
+// class DeviceProvider with ChangeNotifier {
+//   List<Device> devices = [];
+
+//   // Fetch devices from the backend
+//   Future<void> fetchDevices(String roomId, String userId) async {
+//     try {
+//       final response = await http.get(Uri.parse('https://10.0.2.2:8080/api/users/$userId/rooms/$roomId/devices'));
+//       if (response.statusCode == 200) {
+//         final List<dynamic> deviceData = json.decode(response.body);
+//         devices = deviceData.map((device) => Device.fromJson(device)).toList();
+//         notifyListeners();
+//       } else {
+//         throw Exception('Failed to load devices');
+//       }
+//     } catch (error) {
+//       print('Error fetching devices: $error');
+//     }
+//   }
+
+//   // Add a device to the backend
+//   Future<void> addDevice(String roomId, Device device, String userId) async {
+//     try {
+//       final response = await http.post(
+//         Uri.parse('https://10.0.2.2:8080/api/users/$userId/rooms/$roomId/devices'),
+//         headers: {'Content-Type': 'application/json'},
+//         body: json.encode({
+//           'action': 'addDevice',
+//           'data': {'deviceName': device.deviceName},
+//         }),
+//       );
+//       if (response.statusCode == 200) {
+//         devices.add(device);
+//         notifyListeners();
+//       } else {
+//         throw Exception('Failed to add device');
+//       }
+//     } catch (error) {
+//       print('Error adding device: $error');
+//     }
+//   }
+
+//   // Update device status
+//   Future<void> updateDeviceStatus(String roomId, String deviceId, String newStatus, String userId) async {
+//     try {
+//       final response = await http.put(
+//         Uri.parse('https://10.0.2.2:8080/api/users/$userId/rooms/$roomId/devices/$deviceId'),
+//         headers: {'Content-Type': 'application/json'},
+//         body: json.encode({'status': newStatus}),
+//       );
+//       if (response.statusCode == 200) {
+//         final deviceIndex = devices.indexWhere((device) => device.deviceId == deviceId);
+//         if (deviceIndex != -1) {
+//           devices[deviceIndex].status = newStatus;
+//           notifyListeners();
+//         }
+//       } else {
+//         throw Exception('Failed to update device status');
+//       }
+//     } catch (error) {
+//       print('Error updating device status: $error');
+//     }
+//   }
+// }
 }
